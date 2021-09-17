6a65c84a94308997cb0d0ef5ea8678fa12d03c5a
Ishan Randhawa <ishan.randhawa@stashfin.com>
2021-09-18 01:12:39 +0530

    Token POC initial commit

--- server/settings.py
+++ server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,8 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'serverapi',
     'rest_framework',
+        'rest_framework.authtoken',
+
 
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,9 +88,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
+
 
 
 # Password validation
--- serverapi/views.py
+++ serverapi/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets, permissions
+from rest_framework import views, viewsets, permissions, authentication
 from .serializers import serverSerializer
 from .models import server
 
 
 class LeadViewSet(viewsets.ModelViewSet):
+    authentication_classes = [authentication.TokenAuthentication]
     queryset = server.objects.all()
     serializer_class = serverSerializer
+    permission_classes = [permissions.IsCanViewOnly]
+
